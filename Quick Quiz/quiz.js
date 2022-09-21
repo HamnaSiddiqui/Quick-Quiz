469fe644cabd57614a379fd42db2821b23b2dcea
Hamna Siddiqui <hamnas200@gmail.com>
2022-09-21 06:36:24 -0700

    second commit

---
+++ Quick Quiz/quiz.js
@@ -0,0 +1,11 @@
+const quiz = document.getElementById("quiz");
+const question = Array.from(document.getElementsByClassName(""));
+
+let accepting_answers = [];
+let score = 0;
+let question_counter = 0;
+let available_questions = [];
+let current_question = {};
+
+let questions = [
+];
